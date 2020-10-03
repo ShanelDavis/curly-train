2337f8d9c3d8339d45e030c6044971d48b2c49d1
ShanelDavis <nshanelh@gmail.com>
2020-10-03 11:33:28 -0400

    Async and Await

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -323,18 +323,22 @@ const MainNavigator = createDrawerNavigator(
 
 class Main extends Component {
 
+    async showNetInfo() {
+        const connectionInfo = await NetInfo.fetch();
+        (Platform.OS === 'ios') ?
+                Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                    connectionInfo.type, ToastAndroid.LONG);
+    }
+
+
     componentDidMount() {
         this.props.fetchCampsites();
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios') ?
-                Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
+        this.showNetInfo();
 
         this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             this.handleConnectivityChange(connectionInfo);
